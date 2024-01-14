fe75402a378954c6235f6f4210d3d0bd8342cc3d
Amit Masram <amitmasram831@gmail.com>
2024-01-14 23:37:39 +0530

    Shope_app

---
+++ Shope_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shop_example/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'E-Commerce-Store',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+        textTheme: GoogleFonts.mulishTextTheme(),
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
